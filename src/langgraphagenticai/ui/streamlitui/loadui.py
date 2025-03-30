9d9d457e18518294f59790b76fccb26f038634db
Teja <tejaambati20@gmail.com>
2025-03-30 23:11:52 +0530

    Add initial implementation of LangGraph Agentic AI components

---
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import os
+from datetime import date
+
+from langchain_core.messages import AIMessage, HumanMessage
+
+
+class LoadStreamlitUI:
+    def __init__(self):
+        self.config = Config() # Config
+        self.user_controls = {}
+        
+
+
+
+
